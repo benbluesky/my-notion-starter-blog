b92f0c906035bcb7f548292cf5283620eac1d176
Ben <benbluesky@outlook.com>
2023-11-22 10:40:14 +0800

    Update site.config.ts
    fix one notion id bug.

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd89434cb553144acbc3fbd31c02a325d',
+  rootNotionPageId: 'acef925d5e7240748ef76097e954af4e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
